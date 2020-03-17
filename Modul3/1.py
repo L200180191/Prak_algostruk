905257f5f644f29f1b6be030d2b38db6c3ad1e16
L200180191 <48119893+L200180191@users.noreply.github.com>
2020-03-17 23:53:19 +0700

    Add files via upload

--- Modul3/1.py
+++ Modul3/1.py
@@ -1,1 +1,130 @@
+a = [[1,2],[3,4]]
+b = [[5,6],[7,8]]
+c = [[12,3,"y"],[12,33,4]]
+d = [[3,4],[2,4],[1,5]]
+e = [[5,6,7],[7,8,9]]
+f = [[2,3],[4,5,6],[7,8,9]]
+
+def cekKonsis(n):
+    x = len(n[0])
+    y = type(n[0][0])
+    z = 0
+    a = True
+    for i in range (len(n)):
+        for j in range (len(n[i])):
+            c = type(n[i][j])
+            if (c!=y):
+                a = False
+                break
+        if (len(n[i]) == x):
+            z+=1
+        
+    if(z == len(n) and a==True):
+        print("matriks konsisten")
+    else:
+        print("matrik tidak konsisten")
+
+
+
+def cekInt(n):
+    x = 0
+    y = 0
+    for i in n:
+        for j in i:
+            y+=1
+            if (str(j).isdigit()==False):
+                print("tidak semua isi matriks adalah angka")
+                break
+            else:
+                x+=1
+    if(x==y):
+        print("semua isi matriks adalah angka")
+        
+
+
+def ordo(n):
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    print("mempunyai ordo "+str(x)+"x"+str(y))
+
+
+
+def jumlah(n,m):
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    xy = [[0 for j in range(x)] for i in range(y)]
+    
+    z = 0
+    if(len(n)==len(m)):
+        for i in range(len(n)):
+            if(len(n[i]) == len(m[i])):
+                z+=1
+    if(z==len(n) and z==len(m)):
+        print("ukuran sama")
+        for i in range(len(n)):
+            for j in range(len(n[i])):
+                xy[i][j] = n[i][j] + m[i][j]
+        print(xy)
+    else:
+        print("ukuran beda")
+     
+def kali(n,m):
+    aa = 0
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    v,w = 0,0
+    for i in range(len(m)):
+        v+=1
+        w = len(m[i])
+        
+    if(y==v):
+        print("bisa dikalikan")
+        vwxy = [[0 for j in range(w)] for i in range(x)]
+        print(vwxy)
+        for i in range(len(n)):
+            for j in range(len(m[0])):
+                for k in range(len(m)):
+                    vwxy[i][j] += n[i][k] * m[k][j]
+        print(vwxy)
+            
+    else:
+        print("tidak memenuhi syarat")
+
+
+
+
+def determHitung(A, total=0):
+    x = len(A[0])
+    z = 0
+    for i in range(len(A)):
+        if (len(A[i]) == x):
+           z+=1
+    if(z == len(A)):
+        if(x==len(A)):
+            indices = list(range(len(A)))
+            if len(A) == 2 and len(A[0]) == 2:
+                val = A[0][0] * A[1][1] - A[1][0] * A[0][1]
+                return val
+            for fc in indices: 
+                As = A 
+                As = As[1:] 
+                height = len(As) 
+                for i in range(height): 
+                    As[i] = As[i][0:fc] + As[i][fc+1:] 
+                sign = (-1) ** (fc % 2) 
+                sub_det = determHitung(As)
+                total += sign * A[0][fc] * sub_det
+        else:
+            return "tidak bisa dihitung determinan, bukan matrix bujursangkar"
+    else:
+        return "tidak bisa dihitung determinan, bukan matrix bujursangkar"
+    return total
+
+
 
